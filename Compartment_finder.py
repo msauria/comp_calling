f8d4253759e333b04aa198e5ed153813d86ff2de
Michael Sauria <msauria@Michaels-MacBook-Pro.local>
2017-12-05 12:49:12 -0500

    Added MPI support

--- Compartment_finder.py
+++ Compartment_finder.py
@@ -4,9 +4,19 @@
 import sys
 from argparse import ArgumentParser
 
+import time
 import numpy
 import scipy.sparse.linalg
 import hifive
+try:
+  from mpi4py import MPI
+  comm = MPI.COMM_WORLD
+  num_procs = comm.Get_size()
+  rank = comm.Get_rank()
+except:
+  comm = None
+  num_procs = 1
+  rank = 0
 
 def main(args):
   hic = hifive.HiC(args.HIC)
@@ -14,33 +24,75 @@ def main(args):
     args.CHROMS = hic.fends['chromosomes'][...]
   else:
     args.CHROMS = args.CHROMS.split(',')
-  cis_data = {}
-  cis_mapping = {}
+  bounds = {}
   args.CHROMS.sort()
-  chr_indices = [0]
+  new_chr_indices = [0]
+  if 'binned' in hic.__dict__ and hic.binned is not None:
+    fends = hic.fends['bins'][...]
+    chr_indices = hic.fends['bin_indices'][...]
+  else:
+    fends = hic.fends['fends'][...]
+    chr_indices = hic.fends['chr_indices'][...]
   for chrom in args.CHROMS:
-    temp = hic.cis_heatmap(chrom, binsize=args.BINSIZE, datatype='enrichment', arraytype='full', returnmapping=True)
-    if temp is not None:
-      cis_data[chrom] = temp[0]
-      cis_mapping[chrom] = temp[1][:, :2]
-      chr_indices.append(chr_indices[-1] + temp[1].shape[0])
-  args.CHROMS = cis_data.keys()
+    chrint = hic.chr2int[chrom]
+    sfend = chr_indices[chrint]
+    efend = chr_indices[chrint + 1]
+    valid = numpy.where(hic.filter[sfend:efend])[0] + sfend
+    print chrom, valid.shape[0]
+    if valid.shape[0] < 2:
+      print >> sys.stderr, ("Insufficient information for %s\n") % (chrom),
+      continue
+    sbin = (fends['mid'][valid[0]] / args.BINSIZE) * args.BINSIZE
+    ebin = (fends['mid'][valid[-1]] / args.BINSIZE + 1) * args.BINSIZE
+    N = (ebin - sbin) / args.BINSIZE
+    bounds[chrom] = numpy.zeros((N, 2), dtype=numpy.int32)
+    bounds[chrom][:, 0] = numpy.arange(N) * args.BINSIZE + sbin
+    bounds[chrom][:, 1] = bounds[chrom][:, 0] + args.BINSIZE
+    new_chr_indices.append(new_chr_indices[-1] + N)
+  args.CHROMS = bounds.keys()
   args.CHROMS.sort()
+  args = comm.bcast(args, root=0)
+  chr_indices = new_chr_indices
   data = numpy.zeros((chr_indices[-1], chr_indices[-1], 2), dtype=numpy.float64)
   mapping = numpy.zeros((chr_indices[-1], 3), dtype=numpy.int32)
   for i, chrom in enumerate(args.CHROMS):
-    #data[chr_indices[i]:chr_indices[i + 1], chr_indices[i]:chr_indices[i + 1], :] = cis_data[chrom]
-    del cis_data[chrom]
     mapping[chr_indices[i]:chr_indices[i + 1], 0] = i
-    mapping[chr_indices[i]:chr_indices[i + 1], 1:] = cis_mapping[chrom]
-    del cis_mapping[chrom]
-    for j in range(i + 1, len(args.CHROMS)):
-      chrom2 = args.CHROMS[j]
-      trans = hic.trans_heatmap(chrom, chrom2, binsize=args.BINSIZE, start1=mapping[chr_indices[i], 1],
-        stop1=mapping[chr_indices[i + 1] - 1, 2], start2=mapping[chr_indices[j], 1], stop2=mapping[chr_indices[j + 1] - 1, 2],
-        datatype='enrichment')
-      data[chr_indices[i]:chr_indices[i + 1], chr_indices[j]:chr_indices[j + 1], :] = trans
-      data[chr_indices[j]:chr_indices[j + 1], chr_indices[i]:chr_indices[i + 1], :] = numpy.transpose(trans, (1, 0, 2))
+    mapping[chr_indices[i]:chr_indices[i + 1], 1:] = bounds[chrom]
+  indices = list(numpy.triu_indices(len(args.CHROMS), 1))
+  if comm is not None:
+    bounds = comm.bcast(bounds, root=0)
+    node_ranges = numpy.round(numpy.linspace(0, indices[0].shape[0], num_procs + 1)).astype(numpy.int32)
+    for i in range(1, num_procs):
+      comm.send(indices[0][node_ranges[i]:node_ranges[i + 1]], dest=i)
+      comm.send(indices[1][node_ranges[i]:node_ranges[i + 1]], dest=i)
+    indices0 = indices[0][:node_ranges[1]]
+    indices1 = indices[1][:node_ranges[1]]
+  else:
+    indices0 = indices[0]
+    indices1 = indices[1]
+  for i in range(indices0.shape[0]):
+    X = indices0[i]
+    Y = indices1[i]
+    chrom = args.CHROMS[X]
+    chrom2 = args.CHROMS[Y]
+    data[chr_indices[X]:chr_indices[X + 1], chr_indices[Y]:chr_indices[Y + 1], :] = hic.trans_heatmap(
+      chrom, chrom2, binsize=args.BINSIZE, start1=mapping[chr_indices[X], 1], stop1=mapping[chr_indices[X + 1] - 1, 2],
+      start2=mapping[chr_indices[Y], 1], stop2=mapping[chr_indices[Y + 1] - 1, 2], datatype='fend')
+  if comm is not None:
+    for i in range(1, num_procs):
+      for j in range(node_ranges[i], node_ranges[i + 1]):
+        X = indices[0][j]
+        Y = indices[1][j]
+        temp = numpy.zeros((chr_indices[X + 1]- chr_indices[X]) * (chr_indices[Y + 1] - chr_indices[Y]) * 2, dtype=numpy.float32)
+        comm.Recv(temp, source=i, tag=(X*len(args.CHROMS) + Y))
+        data[chr_indices[X]:chr_indices[X + 1], chr_indices[Y]:chr_indices[Y + 1], :] = temp.reshape(chr_indices[X + 1] - chr_indices[X], -1, 2)
+  N = data.shape[0]
+  indices = list(numpy.triu_indices(N, 1))
+  data[indices[1], indices[0], :] = data[indices[0], indices[1], :]
+  valid = numpy.sum(data[:, :, 0], axis=1) > mapping.shape[0] / 2.
+  ivrows = numpy.where(numpy.logical_not(valid))[0]
+  data[ivrows, :, :] = 0
+  data[:, ivrows, :] = 0
 
   if args.PLOT:
     img = hifive.plotting.plot_full_array(data, symmetricscaling=False)
@@ -54,23 +106,43 @@ def main(args):
   scores.sort()
   data[where[0], where[1], 0] = numpy.maximum(scores[int(scores.shape[0]*0.05)], numpy.minimum(scores[int(scores.shape[0]*0.95)],
                                   data[where[0], where[1], 0]))
+  data[where[0], where[1], 0] -= numpy.mean(data[where[0], where[1], 0])
 
-  data2 = numpy.copy(data)
-  data2.fill(0)
-  for i in range(chr_indices[-1] - 1):
-    print >> sys.stderr, ("\r%s\rCorrelating %i of %i bins") % (' '*50, i, chr_indices[-1]),
-    for j in range(i + 1, chr_indices[-1]):
-      try:
-        where = numpy.where((data[i, :, 1] > 0) & (data[j, :, 1] > 0))[0]
-        if where.shape[0] < 3:
-          continue
-        corr = numpy.corrcoef(data[i, where, 0], data[j, where, 0])[0, 1]
-        if corr != numpy.nan and abs(corr) < numpy.inf:
-          data2[i, j, 0] = corr
-          data2[i, j, 1] = 1
-      except:
-        pass
-      data2[j, i, :] = data2[i, j, :]
+  data2 = numpy.zeros(data.shape, dtype=data.dtype)
+  indices[0] = indices[0].astype(numpy.int32)
+  indices[1] = indices[1].astype(numpy.int32)
+  if comm is not None:
+    N = comm.bcast(N, root=0)
+    comm.Bcast(data, root=0)
+    node_ranges = numpy.round(numpy.linspace(0, indices[0].shape[0], num_procs + 1)).astype(numpy.int32)
+    for i in range(1, num_procs):
+      comm.send(node_ranges[i + 1] - node_ranges[i], dest=i)
+      comm.Send(indices[0][node_ranges[i]:node_ranges[i + 1]], dest=i)
+      comm.Send(indices[1][node_ranges[i]:node_ranges[i + 1]], dest=i)
+    indices0 = indices[0][:node_ranges[1]]
+    indices1 = indices[1][:node_ranges[1]]
+  else:
+    indices0, indices1 = indices
+  for i in range(indices0.shape[0]):
+    print >> sys.stderr, ("\r%s\rCorrelating %i of %i bins") % (' '*50, i, indices0.shape[0]),
+    X = indices0[i]
+    Y = indices1[i]
+    try:
+      where = numpy.where((data[X, :, 1] > 0) & (data[Y, :, 1] > 0))[0]
+      if where.shape[0] < N / 10.:
+        continue
+      corr = numpy.corrcoef(data[X, where, 0], data[Y, where, 0])[0, 1]
+      if corr != numpy.nan and abs(corr) < numpy.inf:
+        data2[X, Y, 0] = corr
+        data2[X, Y, 1] = 1
+    except:
+      pass
+  if comm is not None:
+    for i in range(1, num_procs):
+      temp = numpy.zeros((node_ranges[i + 1] - node_ranges[i], 2), dtype=numpy.float64)
+      comm.Recv(temp, source=i)
+      data2[indices[0][node_ranges[i]:node_ranges[i + 1]], indices[1][node_ranges[i]:node_ranges[i + 1]], :] = temp
+  data2[indices[1], indices[0], :] = data2[indices[0], indices[1], :]
   where = numpy.where(data2[:, :, 1])
   scores = data2[where[0], where[1], 0]
   scores.sort()
@@ -81,7 +153,7 @@ def main(args):
   valid = numpy.sum(data2[:, :, 1], axis=1) >= data2.shape[0] / 2
   vrows = numpy.where(valid)[0]
   ivrows = numpy.where(numpy.logical_not(valid))[0]
-  eigen = scipy.sparse.linalg.eigs(data2[vrows, :, 0][:, vrows], k=1)[1][:, 0]
+  eigen = numpy.real(scipy.sparse.linalg.eigs(data2[vrows, :, 0][:, vrows], k=1)[1][:, 0])
 
   output = open("%s.bg" % args.OUTPUT, 'w')
   output1 = open("%s.bed" % args.OUTPUT, 'w')
@@ -113,7 +185,6 @@ def main(args):
     data3 = numpy.zeros((data2.shape[0], data2.shape[0] + 42, 2), dtype=data2.dtype)
     data3[:, 42:, :] = data2
     eigen /= numpy.amax(numpy.abs(eigen)) / 20.5
-    print numpy.amax(eigen), numpy.amin(eigen)
     for i, X in enumerate(vrows):
       data3[X, :40, 1] = 1
       if eigen[i] >= 0:
@@ -124,6 +195,48 @@ def main(args):
     img = hifive.plotting.plot_full_array(data3, logged=False, symmetricscaling=True)
     img.save("%s_comp.png" % args.OUTPUT)
 
+def worker():
+  args = comm.bcast(None, root=0)
+  hic = hifive.HiC(args.HIC)
+  bounds = comm.bcast(None, root=0)
+  indices0 = comm.recv(source=0)
+  indices1 = comm.recv(source=0)
+  for i in range(indices0.shape[0]):
+    X = indices0[i]
+    Y = indices1[i]
+    chrom = args.CHROMS[X]
+    chrom2 = args.CHROMS[Y]
+    data = hic.trans_heatmap(
+      chrom, chrom2, binsize=args.BINSIZE, start1=bounds[chrom][0, 0], stop1=bounds[chrom][-1, 1],
+      start2=bounds[chrom2][0, 0], stop2=bounds[chrom2][-1, 1], datatype='fend')
+    comm.Send(data.flatten(), dest=0, tag=(X * len(args.CHROMS) + Y))
+
+  N = comm.bcast(None, root=0)
+  data = numpy.zeros((N, N, 2), dtype=numpy.float64)
+  comm.Bcast(data, root=0)
+  M = comm.recv(source=0)
+  indices0 = numpy.zeros(M, dtype=numpy.int32)
+  indices1 = numpy.zeros(M, dtype=numpy.int32)
+  comm.Recv(indices0, source=0)
+  comm.Recv(indices1, source=0)
+  data2 = numpy.zeros((M, 2), dtype=numpy.float64)
+  for i in range(indices0.shape[0]):
+    X = indices0[i]
+    Y = indices1[i]
+    try:
+      where = numpy.where((data[X, :, 1] > 0) & (data[Y, :, 1] > 0))[0]
+      if where.shape[0] < N / 2.:
+        continue
+      corr = numpy.corrcoef(data[X, where, 0], data[Y, where, 0])[0, 1]
+      if corr != numpy.nan and abs(corr) < numpy.inf:
+        data2[i, 0] = corr
+        data2[i, 1] = 1
+    except:
+      pass
+  comm.Send(data2, dest=0)
+  del data
+  del data2
+  return None
 
 def generate_parser():
   parser = ArgumentParser(prog='Compartment_finder', description='Find eigenvector-based compartment calls from HiFive Hi-C data.',
@@ -136,6 +249,9 @@ def generate_parser():
   return parser
 
 if __name__ == '__main__':
-  parser = generate_parser()
-  args = parser.parse_args()
-  main(args)
+  if rank == 0:
+    parser = generate_parser()
+    args = parser.parse_args()
+    main(args)
+  else:
+    worker()
